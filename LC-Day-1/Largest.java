d12051378c32c898208b42c6fabd85700a962642
V1shal-dev <vishalpatil5666@gmail.com>
2026-06-08 11:55:12 +0530

    Update Largest.java to include additional variables and enhance logic for finding the largest number among multiple values.

--- LC-Day-1/Largest.java
+++ LC-Day-1/Largest.java
@@ -2,9 +2,17 @@ public class Largest{
     public static void main(String[] args){
         int a = 10;
         int b = 20;
-        int c = a+b;
+        int c = 30;
+        int d = 70;
+        int e = 80;
+        int f = 90;
+        int g = a+b+e+f;
+        int h = a+b+c+d;
+        int i = a*h+h;
 
-        int Largest = Math.max(a, Math.max(b,c));
-        System.out.println(Largest);
+        System.out.println("a: " + a + " b: " + b + " c: " + c + " d: " + d + " e: " + e + " f: " + f + " g: " + g + " h: " + h + " i: " + i);
+
+        int Largest = Math.max(a, Math.max(b, Math.max(c, Math.max(d, Math.max(e, Math.max(f, Math.max(g, Math.max(h, i))))))));
+        System.out.println("Largest number is: " + Largest);
     }
 }
