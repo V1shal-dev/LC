42d6f00a33a0242335c75d8b2d482d57c24ff124
V1shal-dev <vishalpatil5666@gmail.com>
2026-06-08 11:28:49 +0530

    Add initial implementation of the add class with a main method for basic arithmetic operations.

---
+++ LC-Day-1/add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[] args){
+
+        String name = "Vishal";
+        int a = 12;
+        float b = 12.34f;
+        System.out.println(a + b);
+    }
+}
