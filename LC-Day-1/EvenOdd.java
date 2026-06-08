6f9c80a445856b0dff550deee17cdd5fffa3e70b
V1shal-dev <vishalpatil5666@gmail.com>
2026-06-08 11:40:14 +0530

    Add EvenOdd class with main method to determine if a number is even or odd

---
+++ LC-Day-1/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main (String[] args){
+
+        int a = 10;
+        int b= 33;
+
+        if (a % 2 == 0)
+            System.out.println("Number is Even");
+        else
+            System.out.println("Number is Odd");
+    }
+}
