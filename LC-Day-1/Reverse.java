e42a543de4ce3fb40f12745ff70600fe98ececf6
V1shal-dev <vishalpatil5666@gmail.com>
2026-06-08 12:13:55 +0530

    Add Reverse class with main method to reverse a given string and print the result.

---
+++ LC-Day-1/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+    public static void main(String[] args){
+        String a = "Vishal";
+        String reverse = "";
+
+        for(int i = a.length()-1; i >= 0; i--) {
+            reverse += a.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+}
